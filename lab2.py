4cd1e33b16c231b120ca025151535c8ac8ee680f
x74717zz <zhaojian.zhen@student.manchester.ac.uk>
2023-03-15 09:59:29 +0000

    Use fast approximation

--- lab2.py
+++ lab2.py
@@ -4,43 +4,42 @@ import scipy
 import numpy as np
 from matplotlib import pyplot as plt
 
-def threshold(img, T):
-    return np.where(img > T, 255, 0)
+def HarrisPointsDetector(img, window_size, threshold = 50):
+    offset = int((window_size-1)/2)
+    # Find derivatives
+    X = cv2.Sobel(src=img, ddepth=-1, dx=1, dy=0, ksize=3, borderType=cv2.BORDER_REFLECT)
+    Y = cv2.Sobel(src=img, ddepth=-1, dx=0, dy=1, ksize=3, borderType=cv2.BORDER_REFLECT)
+    XY = cv2.Sobel(src=img, ddepth=-1, dx=1, dy=1, ksize=3, borderType=cv2.BORDER_REFLECT)
+    
+    # Apply gaussian
+    gXX = cv2.GaussianBlur(X**2, ksize=(5,5), sigmaX=0.5).astype(np.float32)
+    gYY = cv2.GaussianBlur(Y**2, ksize=(5,5), sigmaX=0.5).astype(np.float32)
+    gXY = cv2.GaussianBlur(XY, ksize=(5,5), sigmaX=0.5).astype(np.float32)
 
-# Open image
-filename = 'images/bernieSanders.jpg'
-img = cv2.imread(filename)
+    # Compute gradient orientation for each pixel
+    orientation = np.arctan2(Y, X) * (180/np.pi)
 
-# Check for success
-if img is None:
-    print('Error: failed to open', filename)
-    sys.exit()
+    # Calculate Harris response
+    harrisResponse = gXX * gYY - gXY**2 - 0.05 * (gXX+gYY)**2
 
-# Convert to greyscale and save it
-grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Get responses > 0
+    harrisResponse[harrisResponse < 0] = 0
 
-# Blur src image
-blur = cv2.GaussianBlur(grey, ksize=(3,3), sigmaX=2)
+    # Normalise 0 - 255
+    harrisResponse = (255*(harrisResponse - np.min(harrisResponse))/np.ptp(harrisResponse))
 
-# Find derivatives
-X = cv2.Sobel(src=blur, ddepth=-1, dx=1, dy=0, ksize=3, borderType=cv2.BORDER_REFLECT)
-Y = cv2.Sobel(src=blur, ddepth=-1, dx=0, dy=1, ksize=3, borderType=cv2.BORDER_REFLECT)
-XY = cv2.Sobel(src=blur, ddepth=-1, dx=1, dy=1, ksize=3, borderType=cv2.BORDER_REFLECT)
-
-# Apply gaussian
-gXX = cv2.GaussianBlur(X**2, ksize=(5,5), sigmaX=0.5)
-gYY = cv2.GaussianBlur(Y**2, ksize=(5,5), sigmaX=0.5)
-gXY = cv2.GaussianBlur(X*Y, ksize=(5,5), sigmaX=0.5)
-
-# Second Moment Matrix for each point
-# Kind of takes a while preferably skip this and just compute Harris response immediately instead
-points = [[[[gXX[x][y], gXY[x][y]], [gXY[x][y], gYY[x][y]]] for y in range(len(gXX[x]))] for x in range(len(gXX))]
-orientation = [[(np.arctan2(Y[x][y], X[x][y]) * (180/np.pi)) for y in range(len(Y[x]))] for x in range(len(Y))]
-
-# Calculate Harris response for each matrix
-harrisResponse =  [[np.linalg.det(M) - 0.05 *(np.trace(M))**2 for M in row] for row in points]
-
-interestPoints = [[response if response > 0 else 0 for response in row] for row in harrisResponse]
+    points, harrisResponse = NMS(harrisResponse, 7)
+    print("Done NMS")
+    
+    kp = []
+    
+    for i in range(len(points)):
+        x, y = points[i][0], points[i][1]
+        if (harrisResponse[x][y] > threshold):
+            kp.append(cv2.KeyPoint(y, x, harrisResponse[x][y], orientation[x][y]))
+    print("Done KP")
+        
+    return kp, harrisResponse
 
 def addBorder(img, pixels):
     output = np.zeros((img.shape[0]+pixels+(pixels*1),img.shape[1]+pixels+(pixels*1)))
@@ -49,14 +48,54 @@ def addBorder(img, pixels):
 
 def NMS(interestPoints, size):
     offset = int((size-1)/2)
-    interestPoints = addBorder(np.array(interestPoints), 3)
-    # suppression = []
-    # for x in range(offset, len(interestPoints)-offset):
-    #     for y in range(offset, len(interestPoints[x])-offset):
-    #         max = max(list(np.concatenate(interestPoints[x-offset:x+offset+1, y-offset:y+offset+1]).flat))
-    #         if (interestPoints[x][y] == max):
-    #             suppression.append([x,y])
-    suppression = [[[x,y] for y in range(offset, len(interestPoints[x])-offset) if (interestPoints[x][y] == max(list(np.concatenate(interestPoints[x-offset:x+offset+1, y-offset:y+offset+1]).flat)))] for x in range(offset, len(interestPoints)-offset)]
-    suppression = list(np.concatenate(suppression).flat)
-    return suppression
+    interestPoints = addBorder(np.array(interestPoints), offset)
+    suppression = []
+    for x in range(offset, len(interestPoints)-offset):
+        for y in range(offset, len(interestPoints[x])-offset):
+                max_val = np.max(interestPoints[x-offset:x+offset+1, y-offset:y+offset+1])
+                if (interestPoints[x][y] == max_val):
+                    interestPoints[x-offset:x+offset+1, y-offset:y+offset+1] = 0
+                    interestPoints[x][y] = max_val
+                    suppression.append([x-offset,y-offset])
+    interestPoints = interestPoints[offset:-offset, offset:-offset]
+    return suppression, interestPoints
+
+def main():
+    # Open image
+    # filename = 'images/bernieSanders.jpg'
+    filename = "images/grey.jpg"
+    # filename = "images/800px-Checkerboard_pattern.svg.png"
+    img = cv2.imread(filename)
+
+    # Check for success
+    if img is None:
+        print('Error: failed to open', filename)
+        sys.exit()
+
+    # Convert to greyscale and save it
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Blur src image
+    blur = cv2.GaussianBlur(grey, ksize=(3,3), sigmaX=2)
+
+    features, responses = HarrisPointsDetector(blur, 2)
+
+    threshold_count = [len(responses[responses > i]) for i in range(256)]
+    
+    # print(responses[0:100])
+    t = plt.figure(1)
+    output = cv2.drawKeypoints(img, features, None, color=(0,255,0), flags=0)
+    plt.imshow(output)
+
+    f = plt.figure(2)
+    plt.plot(threshold_count)
+    plt.xlabel("Threshold")
+    plt.ylabel("Number of Interest Points")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
 
